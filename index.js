0053a4ec783ba2b89b9b42a678656dfec6719942
ersati <piotrp92@gmail.com>
2021-07-01 15:53:50 +0100

    add the logic to search in contact list

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
 // Import stylesheets
 import './style.css';
 
-const people = document.querySelectorAll('.person');
 const input = document.querySelector('input');
+const people = document.querySelectorAll('.person');
 
-console.log(people[0].children[1].textContent, input, people.length);
+// console.log(people[0].children[1].textContent, input, people.length);
 
 input.addEventListener('change', () => {
-  console.log('ok');
-  console.log(people[0].children[1].textContent.indexOf(input.value) > -1);
-  for (let i = 0; i > people.length; i++) {
-    if (people[i].children[1].textContent.indexOf(input.value) > -1) {
-      console.log('hura');
-    } else {
-      console.log('buu');
+  people.forEach(person => {
+    if(person.children[1].textContent.toUpperCase().indexOf(input.value.toUpperCase()) > -1){
+      person.style.display = "";
+    }else {
+      person.style.display = "none";
+      console.log('buu')
     }
-  }
+  })
+
 });
