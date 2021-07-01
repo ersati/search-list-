ea4d58b463f6369262539e421b14127eb24f1c5c
ersati <piotrp92@gmail.com>
2021-07-01 02:00:49 +0100

    add console.log

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ const input = document.querySelector('input');
 console.log(people[0].children[1].textContent, input, people.length);
 
 input.addEventListener('change', () => {
+  console.log('ok');
+  console.log(people[0].children[1].textContent.indexOf(input.value) > -1);
   for (let i = 0; i > people.length; i++) {
     if (people[i].children[1].textContent.indexOf(input.value) > -1) {
       console.log('hura');
