0b26dbed66affe9686675d09da1c80035b961d01
ersati <piotrp92@gmail.com>
2021-07-02 01:51:15 +0100

    add animation to items

--- index.js
+++ index.js
@@ -4,16 +4,19 @@ import './style.css';
 const input = document.querySelector('input');
 const people = document.querySelectorAll('.person');
 
-// console.log(people[0].children[1].textContent, input, people.length);
 
 input.addEventListener('change', () => {
   people.forEach(person => {
-    if(person.children[1].textContent.toUpperCase().indexOf(input.value.toUpperCase()) > -1){
-      person.style.display = "";
-    }else {
-      person.style.display = "none";
-      console.log('buu')
+    if (
+      person.children[1].textContent
+        .toUpperCase()
+        .indexOf(input.value.toUpperCase()) > -1
+    ) {
+      person.style.display = '';
+      person.style.animation = 'out 1s forwards';
+    } else {
+      person.style.animation = 'in 1s forwards';
+      person.style.display = "none"
     }
-  })
-
+  });
 });
